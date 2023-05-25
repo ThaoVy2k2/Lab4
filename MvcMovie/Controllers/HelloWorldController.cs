b2e55e9ae9b710fd93a49009facde19cc3176db7
ThaoVy <134570541+ThaoVy2k2@users.noreply.github.com>
2023-05-25 10:25:36 +0700

    TranNgocThaoVy-205052015

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {//public IActionResult Index()
+	 //{
+	 // return View();
+	 //}
+	 // 
+	 // GET: /HelloWorld/
+		public IActionResult HelloWorld(string name, int numTimes = 1)
+		{
+			ViewData["Message"] = "Hello " + name;
+			ViewData["NumTimes"] = numTimes;
+			return View();
+		}
+
+		// 
+		// GET: /HelloWorld/Welcome/ 
+		public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
+
